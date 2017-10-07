228c8b8bdcd0248841d988d289f9cca6cd7f0bd0
Jappel97 <32598921+Jappel97@users.noreply.github.com>
2017-10-07 16:15:43 -0500

    Basic structure for the login button to do something

--- app/src/main/java/com/example/alexi_000/vitaleyes/SignIn.java
+++ app/src/main/java/com/example/alexi_000/vitaleyes/SignIn.java
@@ -2,6 +2,8 @@ package com.example.alexi_000.vitaleyes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignIn extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Button LogIn = (Button)(findViewById(R.id.LogIn));
+        LogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //login();
+            }
+        })
     }
 }
