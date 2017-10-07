621bb7236041a153b3bcc3a36561f39965dab75a
alexisrolenc <alexis.rolenc@gmail.com>
2017-10-07 18:06:22 -0500

    Changed

---
+++ app/src/main/java/com/example/alexi_000/vitaleyes/AddAPatient.java
@@ -0,0 +1,13 @@
+package com.example.alexi_000.vitaleyes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddAPatient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_apatient);
+    }
+}
