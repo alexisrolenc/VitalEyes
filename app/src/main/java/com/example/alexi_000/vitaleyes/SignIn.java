5d7c99210258bf22ce1da3cc4fce0d0c8dfeaed8
alexisrolenc <alexis.rolenc@gmail.com>
2017-10-07 16:23:18 -0500

    Changed

--- app/src/main/java/com/example/alexi_000/vitaleyes/SignIn.java
+++ app/src/main/java/com/example/alexi_000/vitaleyes/SignIn.java
@@ -1,7 +1,9 @@
 package com.example.alexi_000.vitaleyes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignIn extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+    }
+
+    public void registerButtonPressed (View v)
+    {
+        System.out.println("here*****");
+
+        Intent i = new Intent(this, Register.class);
+        this.startActivity(i);
     }
 }
