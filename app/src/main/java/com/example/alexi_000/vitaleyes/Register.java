de28c93d254ef05c083557e0d5fc442519b3de2f
YzXupoo <mrathalheim@gmail.com>
2017-10-07 19:34:15 -0500

    Changed things

--- app/src/main/java/com/example/alexi_000/vitaleyes/Register.java
+++ app/src/main/java/com/example/alexi_000/vitaleyes/Register.java
@@ -1,7 +1,7 @@
 package com.example.alexi_000.vitaleyes;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
@@ -11,17 +11,24 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
-    public void createAccoun(email, password) {
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (!task.isSuccessful()) {
-                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
+        private FirebaseAuth email;
+        private FirebaseAuth password;
+
+
+        public void createAccount(email, password) {
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                            String userName
+                            //Register here
                         }
-                        //Register here
-                    }
-                });
+                    });
+        }
     }
+
 }
