f3a53c593ef5127a4460e9467740804eebd11b0a
Jappel97 <32598921+Jappel97@users.noreply.github.com>
2017-10-07 19:05:16 -0500

    Basic outline for buttons plus transitions from Home

--- app/src/main/java/com/example/alexi_000/vitaleyes/PartientRoomsMain.java
+++ app/src/main/java/com/example/alexi_000/vitaleyes/PartientRoomsMain.java
@@ -2,6 +2,8 @@ package com.example.alexi_000.vitaleyes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PartientRoomsMain extends AppCompatActivity {
 
@@ -9,5 +11,47 @@ public class PartientRoomsMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_partient_rooms_main);
+
+        Button AddPatientBtn = (Button)(findViewById(R.id.AddPatientBtn));
+        AddPatientBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button AddRoomBtn = (Button)(findViewById(R.id.AddRoomBtn));
+        AddRoomBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button AllBtn = (Button)(findViewById(R.id.AllBtn));
+        AllBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button OccBtn = (Button)(findViewById(R.id.OccBtn));
+        OccBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button VacBtn = (Button)(findViewById(R.id.VacBtn));
+        VacBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
